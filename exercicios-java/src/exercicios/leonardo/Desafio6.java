81af2d8eed0381420f23019c9b0ad29b786b2295
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-10-09 20:56:23 -0300

    Desafio 6 resolvido

---
+++ exercicios-java/src/exercicios/leonardo/Desafio6.java
@@ -0,0 +1,26 @@
+package exercicios.leonardo;
+
+public class Desafio6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+/*Desafio 6: Descobrir porque é exibido o código abaixo:
+ * double nota = 1.3;
+ * if(nota>=9.0);{
+ * 	System.out.println("Quadro de Honra!!");
+ * 	System.out.println("Você é fera!!");
+ * }
+ * 
+*/
+	//Código correto:
+		double nota = 1.3;
+		if (nota>=9) {
+			System.out.println("Quadro de Honra!!!");
+			System.out.println("Você é fera!!");
+		}
+		
+		
+	}
+
+}
