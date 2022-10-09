c6114098e7867e64a8ca1a5173c03db0a8499155
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-10-08 23:17:57 -0300

    Desafio1 resolvido

--- exercicios-java/src/exercicios/leonardo/Desafio1.java
+++ exercicios-java/src/exercicios/leonardo/Desafio1.java
@@ -4,6 +4,20 @@ public class Desafio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+/*Desafio1: Conversão de uma temperatura dada em Fahrenheit
+ 			para Celsius
+ 			
+ 			formula °C = (°F-32) * (5/9)
+ 			*/
+		
+		final double FATOR = 5.0/9.0;
+		
+		double f = 86;
+		double c = (f - 32) * FATOR;
+		
+		System.out.println("Temperatura em graus Celsius: " + c + "°C");
+		
 
 	}
 
